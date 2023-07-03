c5d4b9fb6654d2d373f8e762a9ee6e9399870d33
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-03 14:47:33 +0100

    minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return <div>
     <div className="">
       This is the home page.
-      This is a new line
+      This is a new line. Yeah
     </div>
   </div>;
 }
