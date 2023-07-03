08272151e8bcd895596039bda5c186f079ec56b8
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-07-03 16:27:59 +0100

    login main frame added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section className='flex max-w-5xl mx-auto my-12 items-center space-x-8'>
+      <div className='bg-blue-500 h-24 text-white w-1/2 flex flex-col items-center justify-center space-y-8'>
+        Image Here
+      </div>
+      <div className='bg-blue-500 h-24 text-white w-1/2 flex flex-col items-center justify-center space-y-8'>
+        Login Component goes here
+      </div>
+    </section>
   )
 }
 
